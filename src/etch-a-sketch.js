1dbb38b91b947d821054ec6b102fdfa5c33e9340
Jesse NVD-Tower <jesse+nvd-tower@newvistadigital.com>
2024-01-03 16:00:23 -0700

    feat: set grid size via js

--- src/etch-a-sketch.js
+++ src/etch-a-sketch.js
@@ -1,3 +1,14 @@
-const sketcher = document.getElementById("#sketcher");
-const pixels = document.createElement('div');
-pixels.setAttribute('class', 'pixel')
+const sketcher = document.getElementById("sketcher");
+var gridSize = 16;
+var gridSquare = gridSize * gridSize;
+
+
+// createGrid(gridSize);
+for(var i = 0; i < gridSquare; i += 1) {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  
+  sketcher.appendChild(pixel);
+}
+
+sketcher.setAttribute('style', `width: calc(var(--size-base) * ${gridSize})`);
