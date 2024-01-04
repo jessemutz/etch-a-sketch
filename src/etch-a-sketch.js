bfaf572d4696be612fe5d935701259e3b66da80c
Jesse NVD-Tower <jesse+nvd-tower@newvistadigital.com>
2024-01-03 18:25:25 -0700

    refactor: separate some functions for readability

--- src/etch-a-sketch.js
+++ src/etch-a-sketch.js
@@ -3,19 +3,27 @@ const DEFAULT_COLOR = "var(--brand)"
 
 let currentColor = DEFAULT_COLOR
 let currentGridSize = DEFAULT_GRID_SIZE
-let gridSquare = currentGridSize * currentGridSize
-
 let grid = document.getElementById('grid')
 let btnReset = document.getElementById('reset-button')
 
-grid.setAttribute('style', `width: calc(${currentGridSize} * var(--size-base) )`)
+function initializeGrid() {
+  setGridSize(currentGridSize)
+  createGrid()
+}
+
+function setGridSize(size) {
+  grid.setAttribute('style', `width: calc(${size} * var(--size-base) )`)
+}
 
 // createGrid(DEFAULT_GRID_SIZE);
-for(var i = 0; i < gridSquare; i += 1) {
-  const gridElement = document.createElement('div')
-  gridElement.addEventListener('mouseover', changeColor)
-  gridElement.className = 'pixel'
-  grid.appendChild(gridElement)
+function createGrid() {
+  const gridSquare = currentGridSize * currentGridSize
+  for (var i = 0; i < gridSquare; i += 1) {
+    const gridElement = document.createElement('div')
+    gridElement.addEventListener('mouseover', changeColor)
+    gridElement.className = 'pixel'
+    grid.appendChild(gridElement)
+  }
 }
 
 function changeColor(e) {
@@ -24,9 +32,10 @@ function changeColor(e) {
 
 function resetColor() {
   let pixels = document.querySelectorAll('.pixel')
-  pixels.forEach(Object =>
-    Object.removeAttribute('style')
-  )
+  pixels.forEach(pixel => pixel.removeAttribute('style'))
 }
 
 btnReset.addEventListener('click', resetColor)
+
+// Initialize grid on page load
+initializeGrid();
